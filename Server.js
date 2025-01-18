fe4142dfd9a4ec41132f195850ac09e9f24b2545
john lloyd <casislloyd.com>
2025-01-18 21:35:39 +0800

    asdas

--- Server.js
+++ Server.js
@@ -96,6 +96,7 @@ app.delete('/deleteItem', authenticate, async (req, res) => {
 });
 
 // Start the server
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
