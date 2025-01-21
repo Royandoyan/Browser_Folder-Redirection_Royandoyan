c50d4087ee38fe7db83f9cb881202614a0406a8e
john lloyd <casislloyd.com>
2025-01-21 13:55:02 +0800

    asdsd

--- templates/home.js
+++ templates/home.js
@@ -268,7 +268,7 @@ fileInput.addEventListener('change', async (event) => {
     formData.append('parent_id', parentId);
 
     try {
-        const response = await fetch('https://browser-folder-redirection-royandoyan.onrender.com/upload', {
+        const response = await fetch('https://advance-realtime-folder.onrender.com/upload', {
             method: 'POST',
             body: formData,
         });
