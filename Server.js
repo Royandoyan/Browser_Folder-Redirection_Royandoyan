e479627a0df6fd8735fc715a905b0d8d6cb58563
john lloyd <casislloyd.com>
2025-01-21 14:46:18 +0800

    yu

--- Server.js
+++ Server.js
@@ -12,30 +12,26 @@ const port = 3000;
 
 // Firebase Configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAIKjugxiJh9Bd0B32SEd4t9FImRQ9SVK8",
-  authDomain: "browser-redirection.firebaseapp.com",
-  databaseURL: "https://browser-redirection-default-rtdb.firebaseio.com",
-  projectId: "browser-redirection",
-  storageBucket: "browser-redirection.firebasestorage.app",
-  messagingSenderId: "119718481062",
-  appId: "1:119718481062:web:3f57b707f3438fc309f867",
-  measurementId: "G-RG2M2FHGWV"
-};
-
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-
-cloudinary.config({
-    cloud_name: 'dvkzio03x',  // Correct Cloudinary account
-    api_key: '944258497648494',
-    api_secret: 'mnH7wMY5-x5VAtUL_Mdu6-6t-0w',
-  });
-
+    apiKey: "AIzaSyAIKjugxiJh9Bd0B32SEd4t9FImRQ9SVK8",
+    authDomain: "browser-redirection.firebaseapp.com",
+    databaseURL: "https://browser-redirection-default-rtdb.firebaseio.com",
+    projectId: "browser-redirection",
+    storageBucket: "browser-redirection.firebasestorage.app",
+    messagingSenderId: "119718481062",
+    appId: "1:119718481062:web:3f57b707f3438fc309f867",
+    measurementId: "G-RG2M2FHGWV"
+  };
+  
+  const firebaseApp = initializeApp(firebaseConfig);
+  const db = getFirestore(firebaseApp);
+  
+  cloudinary.config({
+      cloud_name: 'dvkzio03x',  // Correct Cloudinary account
+      api_key: '944258497648494',
+      api_secret: 'mnH7wMY5-x5VAtUL_Mdu6-6t-0w',
+    });
 // Enable CORS for all origins
 app.use(cors());
-
-// Serve static files from the 'templates' directory (assuming index.html is in templates)
-app.use(express.static('templates'));  // Serve templates folder directly
 
 // Multer Configuration for File Handling
 const storage = multer.memoryStorage();
@@ -60,9 +56,8 @@ app.post('/upload', upload.single('file'), (req, res) => {
         cloudinary.uploader.upload_stream(
             { resource_type: 'auto', public_id: publicId },
             async (error, result) => {
-                console.log('Cloudinary upload result:', result);
-                console.log('Cloudinary error:', error);
                 if (error) {
+                    console.error('Cloudinary error:', error);
                     return res.status(500).json({ message: 'Error uploading to Cloudinary.' });
                 }
 
@@ -94,12 +89,6 @@ app.post('/upload', upload.single('file'), (req, res) => {
         res.status(500).json({ message: 'Error during upload.', error: error.message }); // Send JSON error
     }
 });
-
-// Add route to serve the index.html when navigating to the root URL
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/templates/index.html'); // Ensure correct path to the index.html
-});
-
 
 // Start the Server
 app.listen(port, () => {
